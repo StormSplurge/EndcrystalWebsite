ba94c4b20b4303b1b1fb9a1111fbdc9949db6bfb
StormSplurge <StormSplurge@DESKTOP>
2023-05-09 18:59:50 -0400

    added player counter, animated endcrystal logo, client suggestions, and a few other small tweaks. cleaned up code and file structure

---
+++ New ECME Site Version 1/serverstatus.js
@@ -0,0 +1,57 @@
+// Original Author: Alisson Lauffer <alissonvitortc@gmail.com>
+// This code is licensed under the MIT license.
+
+let serverIP = "endcrystal.me";
+
+let infoElem = document.getElementById("server__info");
+let ipElem = document.getElementById("server__ip");
+let motdElem = document.getElementById("server__motd");
+let statusElem = document.getElementById("server__status");
+let versionElem = document.getElementById("server__version");
+
+let updateDetails = (result) => {
+    if (result.online) {
+        statusElem.innerText = "Server Online";
+
+        statusElem.classList.remove("off");
+        statusElem.classList.add("on");
+
+        infoElem.innerText = result.players.online + " " + (result.players.online == 1 ? "/ 50 Players" : "/ 50 Players") + " Online";
+        ipElem.innerText = serverIP;
+
+        versionElem.innerText = result.version;
+        motdElem.innerText = result.motd.clean
+    }
+    else {
+        statusElem.innerText = "Server Offline";
+
+        statusElem.classList.remove("on");
+        statusElem.classList.add("off");
+
+        infoElem.innerText = "Default IP:";
+        ipElem.innerText = serverIP;
+
+        versionElem.innerText = "1.16.5";
+        motdElem.innerText = "";
+    }
+};
+
+
+ipElem.addEventListener("click", () => {
+    navigator.clipboard.writeText(serverIP);
+
+    ipElem.innerText = "Copied!";
+    setTimeout(() => {
+        ipElem.innerText = serverIP;
+    }, 1000);
+});
+
+
+let fetchDetails = () => {
+    fetch("https://api.mcsrvstat.us/2/" + serverIP)
+        .then(response => response.json())
+        .then(data => updateDetails(data));
+}
+fetchDetails();
+
+window.setInterval(fetchDetails(), 30000);
